eb9a5e7003ab585b757c27572838baf20f35b0fd
AnKiWoong <ankiwoong@gmail.com>
2020-05-04 12:31:15 +0900

    23. 글 목록, 글 쓰기, 댓글 쓰기 - 08. Comment 뷰, HTML 만들기

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/', views.posts_list, name='posts_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/write', views.post_write, name='post_write'),
+    path('comment/write', views.comment_write, name='comment_write'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
-from blogs.models import Post
+from blogs.models import Post, Comment
 
 
 # 글 목록
@@ -13,8 +15,9 @@ def posts_list(request):
 # 글 상세
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
+    comments = Comment.objects.filter(post=post.id)     # 댓글 가져오기
 
-    return render(request, 'blogs/post_detail.html', context={'post': post})
+    return render(request, 'blogs/post_detail.html', context={'post': post, 'comments': comments})
 
 # 사용자 인증 여부 체크하기(Decorator)
 @login_required
@@ -38,3 +41,22 @@ def post_write(request):
             return redirect(reverse('post_detail', kwargs={'post_id': post.id}))
 
     return render(request, 'blogs/post_write.html', {'user': request.user, 'errors': errors})
+
+
+@login_required
+def comment_write(request):
+    errors = []
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id', '').strip()
+        content = request.POST.get('content', '').strip()
+
+        if not content:
+            errors.append('댓글을 입력해주세요.')
+
+        if not errors:
+            comment = Comment.objects.create(
+                user=request.user, post_id=post_id, content=content)
+
+            return redirect(reverse('post_detail', kwargs={'post_id': comment.post_id}))
+
+    return render(request, 'blogs/post_detail.html', {'user': request.user, 'errors': errors})
