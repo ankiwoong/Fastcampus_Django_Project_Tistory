cf4a50bd88f8dee27e03d48b1eeae4343c79b6d2
AnKiWoong <ankiwoong@gmail.com>
2020-05-04 17:09:27 +0900

    25. 공감 버튼 누르기 - 02. LikePost 뷰 만들기

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/write', views.post_write, name='post_write'),
     path('comment/write', views.comment_write, name='comment_write'),
+    path('post_like/', views.post_like, name='post_like'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 from blogs.models import Post, Comment
+import json
 
 
 # 글 목록
@@ -16,8 +17,26 @@ def posts_list(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     comments = Comment.objects.filter(post=post.id)     # 댓글 가져오기
+    is_liked = False                                    # 좋아요를 안누른 상태
 
-    return render(request, 'blogs/post_detail.html', context={'post': post, 'comments': comments})
+    if post.likes.filter(id=request.user.id).exists():  # 좋아요를 누른 상태
+        is_liked = True
+
+    return render(request, 'blogs/post_detail.html', context={'post': post, 'comments': comments, 'is_liked': is_liked, 'total_likes': post.total_likes()})
+
+
+@login_required
+@require_POST
+def post_like(request):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    is_liked = post.likes.filter(id=request.user.id).exists()
+
+    if is_liked:
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+
+    return HttpResponseRedirect(reverse('post_detail', kwargs={'post_id': post.id}))
 
 # 사용자 인증 여부 체크하기(Decorator)
 @login_required
