19c60551a4e40bc70062511d260f03032f0594b5
AnKiWoong <ankiwoong@gmail.com>
2020-05-04 12:58:02 +0900

    24. 공감 버튼 누르기 - 01. LikePost, Many to Many

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 from helpers.models import BaseModel
 from users.models import User
@@ -9,9 +10,14 @@ class Post(BaseModel):
     title = models.CharField(max_length=255, blank=False)
     content = models.TextField()
     image = models.ImageField(blank=True, null=Ture)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return '%s - %s' % (self.id, self.title)
+
+    def total_likes(self):
+        return self.likes.count()
 
 
 class Comment(BaseModel):
